cb0b3f29a10811f67e3a4bdd14259f7dea62f11f
AlisherZakir <a3zakir@gmail.com>
2019-06-15 18:35:20 +0100

    implemented timers to improve synchronicity

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,18 +7,23 @@ class RoomsController < ApplicationController
   end
 
   def update
+
     room = Room.find(params[:id])
     game = room.tictactoe
 
     game.next_turn(params[:game][:boxes])
 
-    redirect_to home_path
+    sleep 15
+    redirect_to room_path(room)
   end
 
 
   def show
     @room = Room.find(params[:id])
     @game = @room.tictactoe
+
+
+
     render :"tictactoe/new", layout: "tictactoe"
   end
 
--- app/models/room.rb
+++ app/models/room.rb
@@ -1,4 +1,9 @@
 class Room < ApplicationRecord
   belongs_to :host, class_name: "User"
   has_one :tictactoe
+
+  def curr_player?(user)
+    player = (self.host.username == user ? 0 : 1)
+    self.tictactoe.player == player
+  end
 end
