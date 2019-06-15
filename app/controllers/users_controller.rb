7b482a546bd1633088d4017fb5c645bf868272c5
VladTGeorgiev <vlad.georgiev@outlook.com>
2019-06-15 19:07:17 +0100

    Added username length and uniqueness validation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,8 +3,13 @@ class UsersController < ApplicationController
     @user = User.new
   end
   def create
-    user = User.create(params.require(:user).permit(:username, :password_digest))
-    redirect_to root_path
+    @user = User.create(params.require(:user).permit(:username, :password_digest))
+    if @user.valid?
+      redirect_to root_path
+    else
+      flash[:errors] = @user.errors.full_messages
+      redirect_to new_user_path
+    end
   end
   def show
     @user = User.find(params[:id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+    validates :username, uniqueness: true
+    validates :username, length: { minimum: 3 }
 end
