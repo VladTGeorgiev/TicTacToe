4342b329fa99a3c719b253982d4e3aa182804b75
AlisherZakir <a3zakir@gmail.com>
2019-06-15 20:57:18 +0100

    fixed a merging issue

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,7 @@
 class WelcomeController < ApplicationController
-  skip_before_action :check_id 
+  skip_before_action :check_id
   def index
-    redirect_to home_path if curr_user
+    return redirect_to home_path if curr_user
     render :layout => false
   end
 end
