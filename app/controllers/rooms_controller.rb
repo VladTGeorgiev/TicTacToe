5349606c5f81e34dbb8fbf3e0da26baed3c51347
AlisherZakir <a3zakir@gmail.com>
2019-06-16 22:54:21 +0100

    some refactoring in rooms show

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,19 +1,16 @@
 class RoomsController < ApplicationController
   include RoomsHelper
   def new
-    @opponents = User.order(:username)
   end
 
   def create
-    opp_id = params[:room][:opponent_id]
-    flash[:errors] = []
-    if opp_id.to_i == get_user.id
+    opponent = get_opponent
+    clear_errors
+    if opponent == get_user
       flash[:errors] << "Congradulations, you played yourself"
       return render :new
     end
-    unfinished_game = Room.where(host_id: opp_id).find {|room| room.tictactoe.status == "active"}
-    unfinished_game ||= Room.where(opponent_id: opp_id).find {|room| room.tictactoe.status == "active"}
-    if unfinished_game
+    if opponent.playing?
       flash[:errors] << "This opponent is currently playing with someone else"
       return render :new
     end
@@ -23,23 +20,22 @@ class RoomsController < ApplicationController
   end
 
   def update
-    room = Room.find(params[:id])
-    flash[:errors] ||= []
+    room = get_room
     if !params[:game] || !params[:game][:boxes]
       if room.curr_player?(curr_user)
-        flash[:errors] << "You have to mark at least one box to make a turn"
+        add_error "You have to mark at least one box to make a turn"
       else
-        flash[:errors] << "It is not your turn yet"
+        add_error "It is not your turn yet"
       end
       return redirect_to room_path(room)
     end
     selections = params[:game][:boxes]
     if selections.keys.size > 1
-      flash[:errors] << "You cannot select more than one box"
+      add_error "You cannot select more than one box"
     else
       game = room.tictactoe
       game.next_turn(selections)
-      game.next_player unless game.won? || game.draw?
+      game.next_player unless game.over?
     end
     redirect_to room_path(room)
   end
