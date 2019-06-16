9d7b68b2aec35b8fd30db7fd95aa05e4228551c6
AlisherZakir <a3zakir@gmail.com>
2019-06-16 19:45:08 +0100

    select opponent and check if it is available

--- app/helpers/rooms_helper.rb
+++ app/helpers/rooms_helper.rb
@@ -1,2 +1,15 @@
 module RoomsHelper
+  private
+  def get_user
+    User.find_by(username: curr_user)
+  end
+
+  def concede_prev_games
+    Room.where(host: get_user).select {|room| room.tictactoe.status == "active"}.each {|room| room.tictactoe.update(status: "1")}
+    Room.where(opponent: get_user).select {|room| room.tictactoe.status == "active"}.each {|room| room.tictactoe.update(status: "0")}
+  end
+
+  def create_a_room
+    Room.create(host: get_user, opponent_id: params[:room][:opponent]).create_tictactoe(player: 0, status: "active")
+  end
 end
