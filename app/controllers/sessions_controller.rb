62123a8dfdcbcde88f226a95497c171688ec5b31
AlisherZakir <a3zakir@gmail.com>
2019-06-18 16:48:18 +0100

    redesign of login and sign up pages

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,6 +2,7 @@ class SessionsController < ApplicationController
   skip_before_action :check_id, only: [:new, :create]
   def new
     @user = User.new
+    render layout: "sign_up"
   end
 
   def create
