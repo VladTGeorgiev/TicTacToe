dbae53124f63ca6650cdfc58b286dc1458dac2a0
AlisherZakir <a3zakir@gmail.com>
2019-06-19 12:21:46 +0100

    Hard Ai is implemented

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -44,7 +44,8 @@ class RoomsController < ApplicationController
       unless game.over?
         game.next_player
         if room.opponent.ai
-          selections = HardAi.new.move(game.boxes)
+          cpu = room.opponent.username == "HardAi" ? HardAi.new : EasyAi.new
+          selections = cpu.move(game.boxes)
           game.next_turn(selections)
           game.next_player unless game.over?
         end
