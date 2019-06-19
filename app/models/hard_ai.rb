81371cf69be6a1db4261722c6192321bb74d8553
AlisherZakir <a3zakir@gmail.com>
2019-06-19 10:26:40 +0100

    some more thinking

--- app/models/hard_ai.rb
+++ app/models/hard_ai.rb
@@ -13,7 +13,35 @@ class HardAi
     available_choices
   end
 
-  def calc_best(choice)
+  def calc_best(choices)
+    best_choice = nil
+    choices.each do |choice|
+      return 10 if Tictactoe.new.won?(choice)
+      return 0 if Tictactoe.new.draw?(choice)
+      return calc_best(choice) - 1
+    end
+  end
 
+
+  def min_max(choices)
+  end
+
+  def draw?(board)
+    board.none?(&:nil?)
+  end
+
+  def won_by?(boxes, player)
+    i = player
+    arr = []
+    arr.<<(boxes[0] == i && boxes[0] == boxes[1] && boxes[0] == boxes[2])
+       .<<(boxes[0] == i && boxes[0] == boxes[3] && boxes[0] == boxes[6])
+       .<<(boxes[0] == i && boxes[0] == boxes[4] && boxes[0] == boxes[8])
+       .<<(boxes[1] == i && boxes[1] == boxes[4] && boxes[1] == boxes[7])
+       .<<(boxes[2] == i && boxes[2] == boxes[4] && boxes[2] == boxes[6])
+       .<<(boxes[2] == i && boxes[2] == boxes[5] && boxes[2] == boxes[8])
+       .<<(boxes[3] == i && boxes[3] == boxes[4] && boxes[3] == boxes[5])
+       .<<(boxes[6] == i && boxes[6] == boxes[7] && boxes[6] == boxes[8])
+
+    arr.any?
   end
 end
