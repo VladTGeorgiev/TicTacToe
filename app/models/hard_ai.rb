7251b64900e99e70509726b9d70dfb0309469638
AlisherZakir <a3zakir@gmail.com>
2019-06-19 02:23:00 +0100

    small thinking

--- app/models/hard_ai.rb
+++ app/models/hard_ai.rb
@@ -1,11 +1,19 @@
 class HardAi
   def find_best_move(board)
     choices = get_available_choices(board)
-    
+    best_move = choices[0]
+    choices.each do |choice|
+      calc_best(choice)
+    end
+    best_move
   end
   def get_available_choices(board)
     available_choices = []
     board.each_with_index {|mark, position| available_choices << position if mark.nil?}
     available_choices
+  end
+
+  def calc_best(choice)
+
   end
 end
