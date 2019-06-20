2cce3222a0820566b7cd0b91f7808200875b9423
VladTGeorgiev <vlad.georgiev@outlook.com>
2019-06-20 13:54:59 +0100

    Messages functionality started

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,23 @@
 class MessagesController < ApplicationController
-  def home
-    
+  def index
+    @messages = Message.all
+  end
+
+  def new
+    @message = Message.new
+  end
+
+  def create
+    @message = Message.create(attr)
+    if @message.valid?
+      redirect_to message_path(@message)
+    else
+      flash[:errors] = @message.errors.full_messages
+      render :new
+    end
+  end
+
+  def attr
+    params.require(:message).permit(:title, :text, :from_id, :to_id)
   end
 end
